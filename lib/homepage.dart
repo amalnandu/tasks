b8eabe0342a2323fdb4fc21aeeb25a0b92fccefc
amalnandu <amalnanduwork@gmail.com>
2023-02-02 21:00:51 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,122 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+class homepage extends StatelessWidget {
+  homepage({Key? key}) : super(key: key);
+  final TextEditingController textcontrol = TextEditingController();
+  String info = "";
+  int index = 0;
+
+  Color addColor() {
+    if (index % 2 == 0) {
+      return Color.fromRGBO(237, 238, 238, 0.5);
+    } else
+      return Color.fromRGBO(237, 238, 238, 0.5);
+  }
+
+  void addTask() {
+    FirebaseFirestore.instance
+        .collection("todos")
+        .add({"title": textcontrol.text});
+  }
+
+  onDelete(String id) {
+    FirebaseFirestore.instance.collection("todos").doc(id).delete();
+  }
+
+  Widget buildbody(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            SizedBox(
+              width: 25,
+            ),
+            Expanded(
+                child: TextField(
+              controller: textcontrol,
+            )),
+            SizedBox(
+              width: 5,
+            ),
+            IconButton(
+                onPressed: () {
+                  info = textcontrol.toString();
+                  print(textcontrol.text);
+                  addTask();
+                  index++;
+                },
+                icon: Icon(
+                  Icons.add,
+                  size: 40,
+                )),
+            SizedBox(
+              width: 19,
+            )
+          ],
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        StreamBuilder<QuerySnapshot>(
+            stream: FirebaseFirestore.instance.collection("todos").snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              } else
+                return Expanded(
+                    child: ListView(
+                  children: snapshot.data!.docs.map((document) {
+                    return Dismissible(
+                      background: Container(
+                        color: Colors.red,
+                        child: Icon(
+                          Icons.delete_forever_sharp,
+                          size: 40,
+                        ),
+                      ),
+                      key: Key(document.id),
+                      onDismissed: (direction) {
+                        onDelete(document.id);
+                      },
+                      child: Container(
+                        color: addColor(),
+                        child: ListTile(
+                          title: Text(
+                            document['title'],
+                            style: GoogleFonts.redHatDisplay(
+                                fontSize: 25, color: Colors.black),
+                          ),
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ));
+            })
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        systemOverlayStyle:
+            SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+        backgroundColor: Colors.green,
+        centerTitle: true,
+        title: Text(
+          'Tasks',
+          style: GoogleFonts.outfit(color: Colors.white, fontSize: 30),
+        ),
+      ),
+      body: buildbody(context),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'homepage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: homepage());
+  }
+}
